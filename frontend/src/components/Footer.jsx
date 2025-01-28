d70a34eb686f6a0831ab21268434065a00b613eb
Youssef Ahmed <youssefelnaggar2001@outlook.com>
2025-01-29 01:22:32 +0300

    update the code

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+const Footer = () => {
+    const year = new Date().getFullYear()
+  return (
+    <footer>
+        <Container>
+            <Row>
+                <Col className='text-center py-3'>
+                    <p> CopyRight &copy; ProShop {year} </p>
+                </Col>
+            </Row>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
